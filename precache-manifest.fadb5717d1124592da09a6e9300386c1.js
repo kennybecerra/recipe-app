cc3a76cd6f8683e022d42976f4a51b786bab1360
Kenny Becerra <KennyBecerra.j@gmail.com>
2019-04-07 16:04:50 -0700

    Updates

---
+++ precache-manifest.fadb5717d1124592da09a6e9300386c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4326daf452e41df5705596d24df3e01",
+    "url": "/recipe-app/static/media/search.b4326daf.svg"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  },
+  {
+    "revision": "aa8205a6cfa05c068331",
+    "url": "/recipe-app/static/js/main.a7350942.chunk.js"
+  },
+  {
+    "revision": "3db2e5f614ac8840b43e",
+    "url": "/recipe-app/static/js/2.9ecb3cee.chunk.js"
+  },
+  {
+    "revision": "aa8205a6cfa05c068331",
+    "url": "/recipe-app/static/css/main.e9e41270.chunk.css"
+  },
+  {
+    "revision": "4df000ad4f16b680ee9e323a5040e784",
+    "url": "/recipe-app/index.html"
+  }
+];
